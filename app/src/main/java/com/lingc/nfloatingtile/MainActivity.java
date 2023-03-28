f61a43615e2e5344d1131a8a34f0b74a1caa6e72
jwyh@jwyihao.top <jwyh@jwyihao.top>
2023-03-28 08:14:11 +0800

    .

--- app/src/main/java/com/lingc/nfloatingtile/MainActivity.java
+++ app/src/main/java/com/lingc/nfloatingtile/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.app.ActivityManager;
+import android.content.Context;
 
 import com.lingc.nfloatingtile.util.SpUtil;
 
